49bc929f6e9ba3e2c47fd2cb58baa5616b2f6954
Mrityunjay Kumar <mrityunjaykmr.mk@gmail.com>
2025-09-14 07:36:03 +0530

    addCategory

--- src/main/java/com/eCommerce/controller/AdminController.java
+++ src/main/java/com/eCommerce/controller/AdminController.java
@@ -1,15 +1,28 @@
 package com.eCommerce.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String adminIndex() {
 
         return "admin/index"; // Returns the view name "admin/index"
+    }
+
+    @GetMapping("/addProduct")
+    public String addProduct(){
+
+        return "admin/addProduct"; // Returns the view name "admin/addProduct"
+    }
+
+    @GetMapping("/addCategory")
+    public String addCategory(){
+
+        return "admin/addCategory"; // Returns the view name "admin/addCategory"
     }
 }
